8af9669c5ca0d58743d78ba79ea7e0103b190459
eduarterra1992 <150155961+eduarterra1992@users.noreply.github.com>
2023-11-07 09:41:06 -0500

    Add files via upload

---
+++ PANEL_GR__FICO_-_EDUAR_resources/js/ardoraPanelCFG.js
@@ -0,0 +1,32 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=10; timeIni=10; timeBon=0;
+var successes=0; successesMax=3; attempts=0; attemptsMax=2;
+var score=0; scoreMax=3; scoreInc=1; scoreDec=1
+var typeGame=0;
+var tiTime=true;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="MUY BIEN"; messageTime="INTENTALO NUEVAMENTE"; messageError="MALISIMO"; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var texSel=0; var doneA=["0","0","0"];
+var posX=[415,19,219]; var posY=[43,410,15];
+var coorx=["MjUw", "MTM5", "MjA3"]; var coory=["MjY1", "MjI5", "MTA1"]; var xAnswer=5; answer=[]; rows=6; cols=10; xKey=-1; yKey=-1; animationX=[]; animationY=[]; animationPosX=[]; animationPosY=[]; animationLetter=[];
+var al1=["", "", ""]; var al2=["", "", ""]; var al3=["", "", ""]; var al4=["", "", ""]; answers=["DOOR", "WINDOWS", "ROOF"];indexLetters=0; xSel=-1; ySel=-1; iLetter=-1; jLetter=-1;
+var wordsGame="UEFORUxfR1JfX0ZJQ09fLV9FRFVBUg=="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
